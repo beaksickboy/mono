ba858fa957ae0ec82c3c2e26791beb4fb5f6477b
beaksickboy <ha.pvha.phan@gmail.com>
2021-01-26 22:18:45 +0700

    Update mf

--- hellosecond/container/src/bootstrap.js
+++
@@ -1,8 +0,0 @@
-import { mount } from "Marketing/Bootstrap";
-
-if (process.env.NODE_ENV === "development") {
-  const root = document.querySelector("#root");
-  if (root) {
-    mount(root);
-  }
-}
