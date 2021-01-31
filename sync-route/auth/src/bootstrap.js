89249894241b0fa444f82d36ec7a3abc8bfee4c5
beaksickboy <ha.pvha.phan@gmail.com>
2021-01-31 10:58:56 +0700

    Sync route

--- sync-route/auth/src/bootstrap.js
+++ sync-route/auth/src/bootstrap.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createMemoryHistory, createBrowserHistory } from "history";
+import { createMemoryHistory } from "history";
 
 // Mount function to start up the app
-const mount = (el, { onNavigate, initialEntries, defaultHistory }) => {
-  const history = defaultHistory || createMemoryHistory({ initialEntries });
-  if (onNavigate) {
-    history.listen(onNavigate);
-  }
+const mount = (el, { onNavigate, initialEntries }) => {
+  const history = createMemoryHistory({ initialEntries });
+  console.log(history);
+  history.listen(onNavigate);
 
   ReactDOM.render(<App history={history} />, el);
 
@@ -29,9 +28,9 @@ const mount = (el, { onNavigate, initialEntries, defaultHistory }) => {
 // If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === "development") {
-  const devRoot = document.querySelector("#_marketing-dev-root");
+  const devRoot = document.querySelector("#_auth-dev-root");
   if (devRoot) {
-    mount(devRoot, { defaultHistory: createBrowserHistory() });
+    mount(devRoot);
   }
 }
 
